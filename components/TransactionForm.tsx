6b8f6c4d14f9b943f8e4e20c38806dcf0e239530
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-24 22:03:52 +0100

    Create TransactionForm.tsx

---
+++ components/TransactionForm.tsx
@@ -0,0 +1,8 @@
+export default function TransactionForm() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h2>📝 Create Transaction</h2>
+      <p>Coming soon: Use Safe SDK to create and sign transactions.</p>
+    </div>
+  )
+}
