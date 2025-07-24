65aae0126250a8436af6337d9c46da04d9139c85
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-24 22:05:29 +0100

    Create page.tsx

---
+++ app/owners/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import OwnersPanel from '@/components/OwnersPanel'
+
+export default function OwnersPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>🧑‍🤝‍🧑 Safe Owners</h1>
+      <OwnersPanel />
+    </main>
+  )
+}
