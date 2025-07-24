d8cec5fc5b1d36f328a2188d3230a978059f6214
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-24 22:06:16 +0100

    Create page.tsx

---
+++ app/create/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import TransactionForm from '@/components/TransactionForm'
+
+export default function CreateTxPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>📝 Create Transaction</h1>
+      <TransactionForm />
+    </main>
+  )
+}
