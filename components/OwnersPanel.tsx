482c73628101eba70ba965e61eb439fbb0d5240e
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-24 22:03:15 +0100

    Create OwnersPanel.tsx

---
+++ components/OwnersPanel.tsx
@@ -0,0 +1,8 @@
+export default function OwnersPanel() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h2>🧑‍🤝‍🧑 Safe Owners</h2>
+      <p>Coming soon: View and manage Safe owners.</p>
+    </div>
+  )
+}
