c2b551e9086b525b708ea358eab647b3013a14f4
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-26 00:06:58 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,14 @@
 // pages/_app.tsx
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { WagmiConfig, createConfig, configureChains } from 'wagmi'
-import { mainnet, arbitrum } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
-import {
-  RainbowKitProvider,
-  getDefaultWallets,
-} from '@rainbow-me/rainbowkit'
-import '@rainbow-me/rainbowkit/styles.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { WalletProvider } from "../components/WalletStatus";
 
-const { chains, publicClient } = configureChains(
-  [mainnet, arbitrum],
-  [publicProvider()]
-)
-
-const { connectors } = getDefaultWallets({
-  appName: 'SafeVault 🔐',
-  projectId: '0e86b980-2298-4978-8dd5-0b9e7af95016 ', // optional for WalletConnect v2
-  chains,
-})
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-})
-
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
+    <WalletProvider>
+      <Layout>
         <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  )
+      </Layout>
+    </WalletProvider>
+  );
 }
