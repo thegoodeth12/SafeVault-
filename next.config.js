4a412df8163888ddaf56f8277ba0bf0fd1f02c3a
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-25 04:43:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-// next.config.js
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+module.exports = withPWA({
   reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+});
