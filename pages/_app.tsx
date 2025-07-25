833f336974a42f70a05aaec9f632f86030d9e6b0
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-25 23:09:11 +0100

    Update  _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,38 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
+// pages/_app.tsx
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { mainnet, arbitrum } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+} from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
+
+const { chains, publicClient } = configureChains(
+  [mainnet, arbitrum],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'SafeVault 🔐',
+  projectId: '0e86b980-2298-4978-8dd5-0b9e7af95016 ', // optional for WalletConnect v2
+  chains,
+})
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
