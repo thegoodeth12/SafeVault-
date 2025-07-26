e8b4c5a5a148b1e3aeff513cddab448808b4d64c
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-26 05:13:11 +0100

    Update propose.js
    Signed-off-by: Ethersafe.eth <raphaelchigozle7088@icloud.com>

--- scripts/propose.js
+++ scripts/propose.js
@@ -1,18 +1,15 @@
 const { ethers } = require('ethers');
-const { SafeFactory, EthersAdapter } = require('@safe-global/protocol-kit');
+const { EthersAdapter, SafeFactory } = require('@safe-global/protocol-kit');
 require('dotenv').config();
 
 (async () => {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY, provider);
+  const provider = new ethers.providers.JsonRpcProvider(process.env.https://mainnet.infura.io/v3/d287bc172bba4c66a78315df41afa70c);
+  const signer = new ethers.Wallet(process.env.0e86b980-2298-4978-8dd5-0b9e7af95016, provider);
 
-  const ethAdapter = new EthersAdapter({
-    ethers,
-    signerOrProvider: signer
-  });
-
+  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer });
   const safeFactory = await SafeFactory.create({ ethAdapter });
-  const safe = await safeFactory.connectSafe(process.env.SAFE_ADDRESS);
+
+  const safe = await safeFactory.connectSafe(process.env.0xAfD5f60aA8eb4F488eAA0eF98c1C5B0645D9A0A0);
 
   const txData = {
     to: signer.address,
@@ -21,8 +18,8 @@ require('dotenv').config();
   };
 
   const safeTx = await safe.createTransaction({ transactions: [txData] });
-
   const txHash = await safe.getTransactionHash(safeTx);
   const signature = await safe.signTransactionHash(txHash);
-  console.log(`Proposal created and signed. TxHash: ${txHash}`);
+
+  console.log(`✅ Proposal sent!\nTransaction Hash: ${txHash}`);
 })();
