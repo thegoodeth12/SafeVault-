4441da8157c6f0b95d16df43a82cbf34a952eacd
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-30 18:01:06 +0100

    Create SafeDashboardComponent.tsx
    Signed-off-by: Ethersafe.eth <raphaelchigozle7088@icloud.com>

---
+++ components/SafeDashboardComponent.tsx
@@ -0,0 +1,7 @@
+import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
+
+const { safe } = useSafeAppsSDK();
+
+console.log("Connected Safe Address:", safe.0xAfD5f60aA8eb4F488eAA0eF98c1C5B0645D9A0A0);
+console.log("Chain ID:", safe.Mainnet);
+console.log("Connected App:", safe);
