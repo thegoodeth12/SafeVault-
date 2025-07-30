749050204153653b5f9422c5f22f5d2d18897a8c
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-07-30 18:04:12 +0100

    Update SafeDashboardComponent.tsx
    Signed-off-by: Ethersafe.eth <raphaelchigozle7088@icloud.com>

--- components/SafeDashboardComponent.tsx
+++ components/SafeDashboardComponent.tsx
@@ -1,5 +1,8 @@
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
 
+const user = appKit.getUser();
+console.log("Reown User Address:", user?0xAfD5f60aA8eb4F488eAA0eF98c1C5B0645D9A0A0.const user = appKit.getUser();
+
 const { safe } = useSafeAppsSDK();
 
 console.log("Connected Safe Address:", safe.0xAfD5f60aA8eb4F488eAA0eF98c1C5B0645D9A0A0);
