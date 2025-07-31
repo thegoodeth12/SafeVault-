9f8e816b3b935d9dceebb74c8b9cdd15900464aa
Ethersafe.eth <221436697+thegoodeth@users.noreply.github.com>
2025-07-31 03:32:48 +0100

    Create loadSafes.ts
    Signed-off-by: Ethersafe.eth <221436697+thegoodeth@users.noreply.github.com>

---
+++ utils/loadSafes.ts
@@ -0,0 +1,5 @@
+import metadata from "../data/metadata.json";
+import addressBook from "../data/addressBook.json";
+import restoreBackup from "../data/restoreBackup.json";
+
+// Now you can access metadata.data.addedSafes, etc.
