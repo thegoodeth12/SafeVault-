c2722cdab6f268abc879cdf5b852904f4e7a7683
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-08-03 22:04:30 +0100

    Create jest.config.js
    Signed-off-by: Ethersafe.eth <raphaelchigozle7088@icloud.com>

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['<rootDir>/src/**/*.test.(ts|tsx)'],
+};
